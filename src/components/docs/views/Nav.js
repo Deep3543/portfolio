9cea648b87f6f8e662251c183eeb03bc84688a94
Deep Patel <dmpate@deakin.edu.au>
2021-09-28 17:29:51 +1000

    Simplified the code and polished the Navbar

--- src/components/docs/views/Nav.js
+++ src/components/docs/views/Nav.js
@@ -1,18 +1,30 @@
 import React, {Component} from "react";
-import { Container, Grid, Icon } from "semantic-ui-react";
+import { Container, Grid, GridColumn, Icon } from "semantic-ui-react";
 
 class Nav extends Component{
+    // TODO: Add Responsive Blob Cursor for links
     render(){
         return(
             // Nav Bar 
             <>
-                <div>
-                    <div className="margin-top-4 nav">
-                        <div className="nav-bar-container">
-                            {/* Icon for Theme */}
-                            <Icon name="sun" className="theme-icon"/>
-                        </div>
-                    </div>
+            <div className="nav-container">
+                {/* Navigation Menu Items */}
+                <Grid stackable columns={5}>
+                    <Grid.Column width={1} />
+                    <Grid.Column as='a' href="#">
+                        Home
+                    </Grid.Column>
+                    <Grid.Column as='a' href="#">
+                        Work
+                    </Grid.Column>
+                    <Grid.Column as='a' href="#">
+                        Find Me
+                    </Grid.Column>
+                    <Grid.Column floated="right" width={1}>
+                        {/* Icon for Theme */}
+                        <Icon link name="sun" className="theme-icon" loading/>
+                    </Grid.Column>
+                </Grid>
                 </div>
             </>
         )
