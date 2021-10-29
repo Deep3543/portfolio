6f8ebfe89dff723f99825a3944b8a59e656d7080
Deep Patel <dmpate@deakin.edu.au>
2021-10-29 16:55:30 +1100

    Complete Makeover of the design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Skeleton from './components/docs/Skeleton';
 import Nav from './components/docs/views/Nav';
+import Skel from './components/docs/Skeleton-dif';
 
 function App() {
   return (
     <div className="app">
-      < Nav />
-      < Skeleton />
+      {/* < Nav />
+      < Skeleton /> */}
+      < Skel />
     </div>
   );
 }
