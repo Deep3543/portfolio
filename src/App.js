37d9a52e5dd7466a60f6ba889a188c50406be544
Deep Patel <deeppatel3543@gmail.com>
2021-12-23 11:46:46 +1000

    Minor adjustments in the back design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Skeleton from './components/docs/Skeleton';
 import Work from './components/docs/Work';
 import Find from './components/docs/Find';
 import Skel from './components/docs/Skeleton-dif';
-import {Link, animateScroll as scroll} from 'react-scroll';
 
 
 function App() {
